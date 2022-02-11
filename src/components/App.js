4296fda85fe0b7eab9a7ce79f9bc0e07dcad8321
Ella Tanttu <ellajtanttu@gmail.com>
2022-02-11 10:38:11 -0800

    add static text to app.js for future structure

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      {/* <Header />
+      <About />
+      <Control /> */}
+      <h1>SpaceJuice Inventory Tracker</h1>
+      <p>About SpaceJuice</p>
+      <h4>SpaceJuice Drum Inventory List</h4>
+      <ul>
+        <li>Raspberry Retrograde Drum | Trifid Nebula Co. | Raspberry | 12 canisters left | $18 / Canister</li>
+        <li>Luminous Lime | Hydrus Galaxy Inc. | Lime | 121 canisters left | $24 / Canister </li>
+      </ul>
+      <hr/>
+      <h4>Hydrus Galaxy Drum Detail Page</h4>
+      <ul>
+        <li>Luminous Lime</li>
+        <li>Distributor: Hydrus Galaxy Inc.</li>
+        <li>Flavor: Lime</li>
+        <li>Price: $24 / Canister</li>
+        <li>Canisters Remaining: 121</li>
+        <li>BUTTON: One Canister Sold</li>
+      </ul>
+
+    </React.Fragment>
+  )
+}
+
+export default App;
